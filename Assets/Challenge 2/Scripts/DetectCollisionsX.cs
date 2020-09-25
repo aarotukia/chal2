6d3b6e3df7697dd1f1e199e316930177f8cc3727
aaro <tukiaaro@gmail.com>
2020-09-25 14:14:21 +0300

    challenge 2
    Challenge 2 done + bonus 1 (bonus 2 in progress?)

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    //detects collisions and destroys the collided objects
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+       
+    }
+}
+
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 22;
+    private bool isSpawning;
+    //  private float startDelay = 1.0f;
+    // private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSpawning = false;
+      
+    }
+
+   void Update()
+    {
+     //spawns balls at random intervals between 3 and 5 seconds                      (BONUS 1)
+        if (!isSpawning)
+        {
+            float timer = Random.Range(3, 5);
+            Invoke("SpawnRandomBall", timer);
+            isSpawning = true;
+        }
+
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        isSpawning = false;
+    }
+
+}
